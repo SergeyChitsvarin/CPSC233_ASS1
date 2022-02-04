9cd9e3697c39bc82cad4fd9daad7d48ecdea0872
Sergey <sergeychits20@gmail.com>
2022-02-03 23:01:47 -0700

    adding rowsIn and columnsIn functions

--- src/Test.java
+++ src/Test.java
@@ -8,9 +8,21 @@ public class Test {
         return board;
     }
 
+    public static int rowsIn(int[][] array){
+        int rowCount = array.length;
+        return rowCount;
+    }
+
+    public static int columnsIn(int[][] array){
+        int amountOfOColumns = array[0].length;
+        return amountOfOColumns;
+    }
+
 
     public static void main(String[] args) {
         System.out.println(Test.createBoard(3, 5));
+        System.out.println(Test.rowsIn(Test.createBoard(3, 5)));
+        System.out.println(Test.columnsIn(Test.createBoard(3, 5)));
     }
 }
 
